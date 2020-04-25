5d03cc7047183129c30cb83610e57d09a02d33a8
YuuVR <leyuudachi@hotmail.com>
2020-04-25 20:08:40 +0200

    Added: Main Menu, Credits (WIP), UI Prefab

---
+++ CoronaDodge/Assets/Scripts/QuitGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+
+    public void quitGame()
+    {
+        Application.Quit(); //Quit Game button
+        UnityEditor.EditorApplication.isPlaying = false; //Quit Game in Unity Editor
+    }
+
+}
