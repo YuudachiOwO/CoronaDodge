61bfe6799a5c13a0a0613e8fccf1b813202b88b5
lolesch <leonid.schreiber@yahoo.de>
2020-04-26 14:45:29 +0200

    [Leonid] upgraded camera follow

--- CoronaDodge/Assets/Scripts/CameraFollow.cs
+++ CoronaDodge/Assets/Scripts/CameraFollow.cs
@@ -2,37 +2,50 @@
 
 public class CameraFollow : MonoBehaviour
 {
-	[SerializeField] private Transform playerPos;
+	[SerializeField] private GameObject player;
+	[SerializeField] private Vector3 playerPos;
 
 	// cmaera clamping boarders
-	[SerializeField] private float xMin = -10f;
-	[SerializeField] private float xMax = 10f;
-	[SerializeField] private float zMin = -10f;
-	[SerializeField] private float zMax = 10f;
+	[SerializeField] private float offset = 12.3f;
+	[SerializeField] private float xMin = 0;
+	[SerializeField] private float xMax = 0;
+	[SerializeField] private float zMin = 0;
+	[SerializeField] private float zMax = 0;
 
-	private Vector3 cameraOffset;
+	[SerializeField] private Vector3 cameraOffset;
 
 	[Range(0.01f, 1.0f)]
 	public float smoothSpeed = 0.25f;
 
 	private void Start()
 	{
-		if (!playerPos)
-		{
-			playerPos = transform;
-			throw new MissingComponentException($"{transform.name} is missing a {typeof(Transform)} reference");
-		}
-		cameraOffset = transform.position - playerPos.position;
+		player = GameObject.FindGameObjectWithTag("Player");
+		if (!player)
+			throw new MissingComponentException("missing player reference");
+		playerPos = player.transform.position;
+
+		cameraOffset = GetComponentInChildren<Camera>().transform.position - playerPos;
+
+		xMax = offset;
+		xMin = -offset;
+		zMax = playerPos.z + xMax;
+		zMin = playerPos.z - xMax;
 	}
 
 	private void FixedUpdate()
 	{
+		playerPos = player.transform.position;
 		// save the clamped borders in local variables
-		var camClampX = Mathf.Clamp(playerPos.position.x, xMin, xMax);
-		var camClampZ = Mathf.Clamp(playerPos.position.z, zMin, zMax);
+		var camClampX = Mathf.Clamp(playerPos.x, xMin, xMax);
+		zMax = playerPos.z + xMax;
+		if (playerPos.z > zMin + xMax)
+		{
+			zMin = playerPos.z - xMax;
+		}
+		var camClampZ = Mathf.Clamp(playerPos.z, zMin, zMax);
 
 		// set the target vector inside the clamped values and slerp to it
-		Vector3 targetPos = new Vector3(camClampX, playerPos.position.y, camClampZ) + cameraOffset;
+		Vector3 targetPos = new Vector3(camClampX, playerPos.y, camClampZ);
 		transform.position = Vector3.Slerp(transform.position, targetPos, smoothSpeed);
 	}
 }
