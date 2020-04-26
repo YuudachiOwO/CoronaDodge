f1614ac3aab0b2461cc0872c583baccd685f4ced
lolesch <leonid.schreiber@yahoo.de>
2020-04-26 11:38:44 +0200

    [Leonid] CoronaColor - fixed the enemy tracking and color gradient

--- CoronaDodge/Assets/Scripts/CoronaColor.cs
+++ CoronaDodge/Assets/Scripts/CoronaColor.cs
@@ -2,16 +2,22 @@
 
 public class CoronaColor : MonoBehaviour
 {
-	[SerializeField] private Transform enemy;
+	//private Transform enemy;
 	private CapsuleCollider coronaCollider;
 	private SpriteRenderer circle;
 	private float distance;
+
 	[SerializeField] private float minCollisionDist = 1.5f;
 
 	[SerializeField] private Color baseColor = new Color(1, 1, 1, 1);
 	[SerializeField] private Color coronaColor = new Color(.75f, .25f, 0, 1);
 	private float percentage = 1f;
-	// Start is called before the first frame update
+
+	// new version
+	private float shortestDist;
+	[SerializeField] private Transform[] enemies;
+	private int enemyCounter = 0;
+
 	void Start()
 	{
 		coronaCollider = GetComponent<CapsuleCollider>();
@@ -27,16 +33,27 @@ public class CoronaColor : MonoBehaviour
 	}
 
 	// Update is called once per frame
-	void Update()
+	void FixedUpdate()
 	{
-		if (enemy != null)
+		if (enemies.Length != 0)
 		{
-			
-			distance = Vector3.Distance(transform.position, enemy.position) - minCollisionDist;
-
-			percentage = distance / coronaCollider.radius;
+			shortestDist = coronaCollider.radius;
+			foreach (var enemy in enemies)
+			{
+				float enemyDistance = (Vector3.Distance(enemy.position, transform.position) - minCollisionDist);
+				if (enemyDistance < shortestDist)
+				{
+					shortestDist = enemyDistance;
+					distance = shortestDist;
+				}
+			}
+			print(shortestDist);
+			percentage = distance / (coronaCollider.radius - minCollisionDist);
 		}
-
+		else
+		{
+			percentage = 1;
+		}
 		circle.color = Color.Lerp(coronaColor, baseColor, percentage);
 	}
 
@@ -44,12 +61,38 @@ public class CoronaColor : MonoBehaviour
 	{
 		if (other.tag == "Enemy")
 		{
-			enemy = other.transform;
+			enemyCounter++;
+
+			var temp = enemies;
+			enemies = new Transform[enemyCounter];
+			for (int i = 0; i < temp.Length; i++)
+			{
+				enemies[i] = temp[i];
+			}
+			enemies[enemyCounter - 1] = other.transform;
 		}
 	}
 
 	private void OnTriggerExit(Collider other)
 	{
-		enemy = null;
+		if (other.tag == "Enemy")
+		{
+			enemyCounter--;
+
+			var temp = new Transform[enemyCounter];
+			if (temp.Length != 0)
+			{
+				var count = 0;
+				for (int i = 0; i < enemies.Length; i++)
+				{
+					if (enemies[i] != other)
+					{
+						temp[count] = enemies[i];
+						count++;
+					}
+				}
+			}
+			enemies = temp;
+		}
 	}
 }
