1ba667faaabe6f00c31afdbf576383ff50a275d4
YuuVR <leyuudachi@hotmail.com>
2020-04-26 02:02:24 +0200

    WIP, healtbar. Added Andreas LevelManager & LevelHandler script

---
+++ CoronaDodge/Assets/Scripts/LevelHandle.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AsserTOOLres {
+	public class LevelHandle : MonoBehaviour {
+		public GameObject[] nextLevels;
+		public float levelLength;
+	}
+}
